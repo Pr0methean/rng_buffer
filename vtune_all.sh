3a3622af36e191f1f613f4b05346256c47dc8736
Chris Hennick <hennickc@amazon.com>
2024-01-23 18:10:09 -0800

    Bug fix: wrong result folder

--- vtune_all.sh
+++ vtune_all.sh
@@ -6,7 +6,7 @@ cd /
 BENCHES=( os buffer_size_2 buffer_size_4 buffer_size_8 buffer_size_16 )
 for BENCH in "${BENCHES[@]}"; do
   mkdir "$BENCH"-results
-  vtune -collect uarch-exploration -result-dir=/results -- /"$BENCH"
+  vtune -collect uarch-exploration -result-dir="$BENCH"/results -- /"$BENCH"
   for FILE in $BENCH-results/*; do
     aws s3api put-object --bucket rng-buffer-reports --body "$FILE" --key "$FILE"
   done
