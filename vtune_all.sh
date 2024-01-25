fde4ceb03bd71a43ffe4f0cfa3e0f6ca3972d304
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-01-25 10:12:31 -0800

    Bug fixes for vtune_all.sh

--- vtune_all.sh
+++ vtune_all.sh
@@ -13,8 +13,8 @@ cd /
 BENCHES=( os buffer_size_2 buffer_size_4 buffer_size_8 buffer_size_16 )
 for BENCH in "${BENCHES[@]}"; do
   mkdir "$BENCH"-results
-  vtune -collect uarch-exploration -result-dir="$BENCH"/results -- /"$BENCH"
-  for FILE in $BENCH-results/*; do
-    aws s3api put-object --bucket rng-buffer-reports --body "$FILE" --key "$FILE"
+  /opt/intel/oneapi/vtune/2024.0/bin64/vtune -collect uarch-exploration -result-dir="$BENCH"-results -- /"$BENCH"
+  for FILE in "$BENCH"-results/*; do
+    aws s3api put-object --bucket rng-buffer-reports --body "$BENCH"-results/$FILE" --key "$FILE"
   done
 done
