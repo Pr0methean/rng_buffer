37de6c4bc244569bedf64ba75d17bf091af6f9ae
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-01-25 10:29:25 -0800

    Clean up old results

--- vtune_all.sh
+++ vtune_all.sh
@@ -13,8 +13,9 @@ cd /
 BENCHES=( os buffer_size_2 buffer_size_4 buffer_size_8 buffer_size_16 )
 for BENCH in "${BENCHES[@]}"; do
   mkdir "$BENCH"-results
+  rm "$BENCH"-results/*
   /opt/intel/oneapi/vtune/latest/bin64/vtune -collect uarch-exploration -result-dir="$BENCH"-results -- /"$BENCH"
   for FILE in "$BENCH"-results/*; do
-    aws s3api put-object --bucket rng-buffer-reports --body "$BENCH"-results/"$FILE" --key "$FILE"
+    aws s3api put-object --bucket rng-buffer-reports --body /"$BENCH"-results/"$FILE" --key "$FILE"
   done
 done
