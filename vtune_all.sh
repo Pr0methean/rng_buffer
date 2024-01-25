92229f233d7a23d0c4e018304506d6703273000e
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-01-25 10:16:14 -0800

    Bug fix: missing quote

--- vtune_all.sh
+++ vtune_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt/intel/oneapi/vtune/2024.0/sepdk/src || exit 1
+cd /opt/intel/oneapi/vtune/latest/sepdk/src || exit 1
 ./insmod-sep -g vtune -pu
 ./boot-script --install
 cd /
@@ -13,8 +13,8 @@ cd /
 BENCHES=( os buffer_size_2 buffer_size_4 buffer_size_8 buffer_size_16 )
 for BENCH in "${BENCHES[@]}"; do
   mkdir "$BENCH"-results
-  /opt/intel/oneapi/vtune/2024.0/bin64/vtune -collect uarch-exploration -result-dir="$BENCH"-results -- /"$BENCH"
+  /opt/intel/oneapi/vtune/latest/bin64/vtune -collect uarch-exploration -result-dir="$BENCH"-results -- /"$BENCH"
   for FILE in "$BENCH"-results/*; do
-    aws s3api put-object --bucket rng-buffer-reports --body "$BENCH"-results/$FILE" --key "$FILE"
+    aws s3api put-object --bucket rng-buffer-reports --body "$BENCH"-results/"$FILE" --key "$FILE"
   done
 done
